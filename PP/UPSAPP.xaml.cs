7e35603ffff61f09264a05f488155e34ee5a25d9
Гриша Шалашов <g.shalashov3@mail.ru>
2022-12-20 01:17:51 +0700

    Доработка

--- PP/UPSAPP.xaml.cs
+++ PP/UPSAPP.xaml.cs
@@ -20,30 +20,84 @@ namespace PP.Forms
     /// </summary>
     public partial class UPSAPP : Window
     {
-        MFCEntities2 MFC;
+        MFCEntities2 MFC { get; set; }
         public UPSAPP()
         {
             MFC = new MFCEntities2();
             InitializeComponent();
-            IBP_List.ItemsSource = MFC.UPSU.ToList();
-            cbStatus.ItemsSource = MFC.Stat.ToList();
-            cbFilter.ItemsSource = MFC.Stat.ToList();
-            cbUPS.ItemsSource = MFC.UPSMod.ToList();
+            IBP.ItemsSource = MFC.UPSU.ToList();
+            sStatus.ItemsSource = MFC.Stat.ToList();
+            SbFilter.ItemsSource = MFC.Stat.ToList();
+            SUPS.ItemsSource = MFC.UPSMod.ToList();
         }
 
         private void btnAdd(object sender, RoutedEventArgs e)
         {
+            UPSU ups = new UPSU();
 
+            //Добавление вводимых данных в базу
+            var model = SUPS.SelectedItem as UPSMod;
+            ups.UPSModel = model.ID;
+            ups.SerialNum = SSerial.Text;
+            ups.InvNum =SInventory.Text;
+            var upsStatus = sStatus.SelectedItem as Stat;
+            ups.Status = upsStatus.ID;
+
+
+
+            MessageBox.Show("ИБП успешно добавлен в базу!");
+
+            try
+            {
+                MFC.UPSU.Add(ups);
+                MFC.SaveChanges();
+                IBP.ItemsSource = MFC.UPSU.ToList();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            SUPS.SelectedValue = -1;
+            SSerial.Clear();
+            SInventory.Clear();
+            sStatus.SelectedValue = -1;
         }
 
         private void btnBack(object sender, RoutedEventArgs e)
         {
-
+            MainWindow mn = new MainWindow();
+            mn.Show();
+            Close();
         }
 
         private void btnEdit(object sender, RoutedEventArgs e)
         {
+            int num = Convert.ToInt32(tbID.Text);
+            var eRow = MFC.UPSU.Where(x => x.ID == num).FirstOrDefault();
+            var model = SUPS.SelectedItem as UPSMod;
+            eRow.UPSModel = model.ID;
+            eRow.SerialNum = SSerial.Text;
+            eRow.InvNum = SInventory.Text;
+            var Status = sStatus.SelectedItem as Stat;
+            eRow.Status = Status.ID;
+            MessageBox.Show("Данные обновлены!");
 
+            try
+            {
+                MFC.SaveChanges();
+                IBP.ItemsSource = MFC.UPSU.ToList();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+            SUPS.SelectedValue = -1;
+            SSerial.Clear();
+            SInventory.Clear();
+            sStatus.SelectedValue = -1;
         }
 
         private void filterCB(object sender, SelectionChangedEventArgs e)
